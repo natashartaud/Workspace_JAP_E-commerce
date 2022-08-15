1e875b57839eb0da1137e334265033b867a84288
natashartaud <natashagonzalez@gmail.com>
2022-08-15 20:50:37 -0300

    Autenticacion Google / Prueba 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 function validateForm() {
+
     let name    = document.forms["myFormLogin"]["name"].value;
     let psw     = document.forms["myFormLogin"]["psw"].value;
     emailInput  = document.getElementById("emailInput");
@@ -18,11 +19,26 @@ function validateForm() {
         }
         if (psw == "") {
             pswInput.classList.add("imputError");
-            //pswMensaje.style.display = "block";
+            pswMensaje.style.display = "block";
         }
         return false;
     }
 }
+
+function onSignIn(googleUser) {
+    
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      }
 
 
 
