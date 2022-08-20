0dadfda78e3fef45a35e737b7f11a9447269ce02
natashartaud <natashagonzalez@gmail.com>
2022-08-19 22:14:01 -0300

    nueva metodo de sigin in google

--- js/index.js
+++ js/index.js
@@ -25,23 +25,7 @@ function validateForm() {
     }
 }
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    var id_token = googleUser.getAuthResponse().id_token;
-}
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-}
-  
-function onLoad() {
-    gapi.load('auth2', function() {
-     gapi.auth2.init();
-    });
-}
 
 
 
